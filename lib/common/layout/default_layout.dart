85ce1f5299757dd8073b6b3dfae7f33e6a80820d
hiroki0h <50644511+hiroki0h@users.noreply.github.com>
2023-08-01 16:12:37 +0900

    SplashScreen 구현

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
+  final Color? backgroundColor;
   final Widget child;
-  const DefaultLayout({required this.child, super.key});
+  const DefaultLayout({required this.child, this.backgroundColor, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white, body: child);
+    return Scaffold(
+        backgroundColor: backgroundColor ?? Colors.white, body: child);
   }
 }
--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:actual/common/layout/default_layout.dart';
 
 class RootTab extends StatelessWidget {
   const RootTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
+    return const DefaultLayout(
+        child: Center(
       child: Text('Root Tab'),
     ));
   }
