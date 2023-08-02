1176a7f3b89a74ba6d83e7b6a2cae5b4150cb7dc
hiroki0h <50644511+hiroki0h@users.noreply.github.com>
2023-08-02 11:33:55 +0900

    RestaurantCard 작업하기

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,3 +1,4 @@
+import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:flutter/material.dart';
 
 class RestaurantScreen extends StatelessWidget {
@@ -6,7 +7,21 @@ class RestaurantScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Center(child: Text('home')),
+      child: Center(
+        child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: RestaurantCard(
+                image: Image.asset(
+                  'asset/img/food/ddeok_bok_gi.jpg',
+                  fit: BoxFit.cover, // 이미지 전체
+                ),
+                name: 'ddeok_bok_gi',
+                tags: const ['aaa', 'bbb', 'ccc'],
+                ratingsCount: 100,
+                deliveryTime: 15,
+                deliveryFee: 2000,
+                ratings: 4.52)),
+      ),
     );
   }
 }
