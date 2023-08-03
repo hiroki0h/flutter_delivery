271784db389d122d606498b1c2adc5febf3a7586
hiroki0h <50644511+hiroki0h@users.noreply.github.com>
2023-08-03 14:39:03 +0900

    Route 연결

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,4 +1,5 @@
 import 'package:actual/restaurant/model/restaurant_model.dart';
+import 'package:actual/restaurant/view/restaurant_detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:actual/common/const/data.dart';
 import 'package:dio/dio.dart';
@@ -24,38 +25,37 @@ class RestaurantScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
           child: FutureBuilder<List>(
-              future: paginateRestaurant(),
-              builder: (context, AsyncSnapshot<List> snapshot) {
-                if (!snapshot.hasData) {
-                  return Container();
-                }
-                return ListView.separated(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (_, index) {
-                    final item = snapshot.data![index];
-                    final pItem = RestaurantModel.fromJson(json: item);
-                    return RestaurantCard(
-                      image: Image.network(
-                        pItem.thumbUrl,
-                        fit: BoxFit.cover, // 이미지 전체
-                      ),
-                      // image: Image.asset(
-                      //   'asset/img/food/ddeok_bok_gi.jpg',
-                      //   fit: BoxFit.cover, // 이미지 전체
-                      // ),
-                      name: pItem.name,
-                      tags: pItem.tags,
-                      ratingsCount: pItem.ratingsCount,
-                      deliveryTime: pItem.deliveryTime,
-                      deliveryFee: pItem.deliveryFee,
-                      ratings: pItem.ratings,
-                    );
-                  },
-                  separatorBuilder: (_, index) {
-                    return const SizedBox(height: 16.0);
-                  },
-                );
-              }),
+            future: paginateRestaurant(),
+            builder: (context, AsyncSnapshot<List> snapshot) {
+              if (!snapshot.hasData) {
+                return Container();
+              }
+              return ListView.separated(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (_, index) {
+                  final item = snapshot.data![index];
+                  final pItem = RestaurantModel.fromJson(
+                    json: item,
+                  );
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (_) => const RestaurantDetailScreen(),
+                        ),
+                      );
+                    },
+                    child: RestaurantCard.fromModel(
+                      model: pItem,
+                    ),
+                  );
+                },
+                separatorBuilder: (_, index) {
+                  return const SizedBox(height: 16.0);
+                },
+              );
+            },
+          ),
         ),
       ),
     );
